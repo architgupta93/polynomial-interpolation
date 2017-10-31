f6ebf81a18c84a092af6f793bb0ec7264f3f47b1
Archit Gupta <architgupta@berkeley.edu>
2017-10-30 20:15:05 -0700

    implementing-3d-interpolants: Added and tested 2D piecewise BLI code
    Changes committed:
    	new file:   bli/PiecewiseBLI2.m
    	modified:   examples/piecewise/fx_pw_bli.m
    	new file:   examples/piecewise/fx_pw_bli2.m

--- matlab/examples/piecewise/fx_pw_bli2.m
+++ matlab/examples/piecewise/fx_pw_bli2.m
@@ -1,14 +1,14 @@
-n_dims         = 1;
+n_dims         = 2;
 n_pieces       = 4;
 
 disp('Example illustrating the use of Barycentric-Lagrange Interpolant.');
 fun            = getTestFHandle(n_dims, 1, 'smooth');
 
 disp('Generating arguments for the interpolant.');
-args           = defaultPiecewiseInterpolantArgs(1);
+args           = defaultPiecewiseInterpolantArgs(2);
 
 disp('Instantiating BLI.')
-bl_interpolant = PiecewiseBLI (fun, args{:});
+bl_interpolant = PiecewiseBLI2(fun, args{:});
 
 n_test_pts     = 100;
 bounds         = args{2};
