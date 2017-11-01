6c3ccbdde16e36d02dad4fe54ec1136a8ca778cc
ag <architgupta@berkeley.edu>
2017-10-31 21:51:29 -0700

    master: Added documentation for the utility script for getting a test function handle
    Changes committed:
    	modified:   getTestFHandle.m

--- matlab/utils/getTestFHandle.m
+++ matlab/utils/getTestFHandle.m
@@ -3,8 +3,37 @@ function [f_handle, df_handle, bkpts] = getTestFHandle(n_in_dims, n_op_dims, f_s
 % Author: Archit Gupta, March 03, 2017
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Takes number of input dimensions and the number of output dimensions as a function and creates a function handle for a
-% function that produces a vector ouptut where the size of the output is given by the parameter n_op_dims
+% Takes number of input dimensions and the number of output dimensions as a
+% function and creates a function handle for a function that produces a vector
+% ouptut where the size of the output is given by the parameter n_op_dims
+% INPUT(s):
+%   n_in_dims: [SCALAR] Number of input dimensions
+%   n_op_dims: [SCALAR] Number of output dimensions
+%   f_seed: Seed for the function to be chosen for test. Options are:
+%       - 'c': Continuous (Has a derivative discontinuity)
+%       - 'd': Discontinuous (Has a value discontinuity)
+%       - 'g': Gaussian (Default. Chosen if an unrecognized 'f_seed' is supplied)
+%       - 'o': Oscillatory/Periodic (Sinusoidal function)
+%       - 'p': Product Peak (See help f_product_peak)
+%       - 'r': Corner Peak (See help f_corner_peak)
+%       - 'smooth': one of the smooth functions mentioned above
+%
+% OUTPUT(s):
+%   f_handle: [FUNCTION HANDLE] the function to be tested. This will be one of
+%       the functions described in the f_seed description above.
+%   df_handle: [FUNCTION HANDLE] Handle for the derivative of the function
+%       supplied as f_handle
+%   bkpts: [ARRAY] If the supplied function is discontinuous, then bkpts
+%       returns the breakpoints, or points of discontinuity
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% EXAMPLE:
+%   1. Checking out BLI interpolation using test function(s)
+%   [fun, dfun] = getTestFHandle(1, 1, 'smooth');
+%   args        = defaultInterpolantArgs(1);
+%   bli_ip      = BLI(fun, args{:});
+%   compareIObjs(10, args{2}, fun, bli_ip);
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
