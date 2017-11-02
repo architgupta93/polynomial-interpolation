a36fea5773bb94bcc00dd3914090ef50c9c22662
Archit Gupta <architgupta@berkeley.edu>
2017-11-01 20:14:21 -0700

    smoothing-functions: Implemented derivative matching using a smooth step
    Changes committed:
    	modified:   examples/smoothing/fx_smoothstep.m
    	modified:   smooth/SmoothStep.m

--- matlab/examples/smoothing/fx_smoothstep.m
+++ matlab/examples/smoothing/fx_smoothstep.m
@@ -1,14 +1,21 @@
 disp('Example illustrating the use of Barycentric-Lagrange Interpolant.');
 % fun            = getTestFHandle(1, 1, 'smooth');
-step_width      = 10000;
-fun             = @(x) tanh(step_width*x);
-dfun            = @(x) step_width*(1 - tanh(step_width*x).^2);
+
+step_width      = 1;
+fun             = @(x) abs(x) .* tanh(step_width*x);
+dfun            = @(x) (sign(x) .* tanh(step_width*x)) + ...
+                    (step_width * abs(x) .* (1 - tanh(step_width*x).^2));
 
 disp('Generating arguments for the interpolant.');
 args           = defaultInterpolantArgs();
 
 disp('Instantiating BLI.')
 bl_interpolant = SmoothStep(fun, args{:});
+
+% Get the derivatives
+l_derv         = dfun(args{2}{1}(1));
+r_derv         = dfun(args{2}{1}(end));
+coeffs         = bl_interpolant.generateCoeffs(l_derv, r_derv)
 
 n_test_pts     = 1000;
 bounds         = args{2};
--- matlab/smooth/SmoothStep.m
+++ matlab/smooth/SmoothStep.m
@@ -13,36 +13,47 @@ classdef SmoothStep < Interpolant
             Obj = Obj@Interpolant(f_vals, in_dims, bounds, order, ...
                 i_type_or_x_vals);
 
-            % Create coefficients
-            % Hardcoding the order to 4 in current case
-            Obj.coeffs = zeros([Obj.op_dims 4]);
-
-            % Let's start with an actual step function for which the value
-            % of the derivative at the boundary points is 0.
-            %
             % The domain is [-1, 1]; rescaling the actual domain to this has
             % already been handled above.
-            %
+            Obj.generateCoeffs([], []);
+        end 
+
+        function coeffs = generateCoeffs(Obj, vp_l, vp_r)
             % The scalar equations for this would be:
             %   p(x)   = ax3 + bx2 + cx + d
             %   p'(-1) = 3a - 2b + c        ... vp_l
             %   p'(1)  = 3a + 2b + c        ... vp_r
             %   p(-1)  = -a + b - c + d     ... v_l
             %   p(1)   =  a + b + c + d     ... v_r
-            %
-            % Zero derivatives at the boundaries already gives us:
-            %   b      = 0
-            %   c      = -3a
-            %   d      = (p(1) + p(-1))/2
-            %   a      = (d - p(1))/2 
+            if (isempty(vp_l))
+                vp_l = zeros(Obj.op_dims);
+            end
+
+            if (isempty(vp_r))
+                vp_r = zeros(Obj.op_dims);
+            end
 
             np  = Obj.getNPts();
             v_l = Obj.f_vals(Obj.colons{:}, np);
             v_r = Obj.f_vals(Obj.colons{:}, 1);
-            Obj.coeffs(Obj.colons{:}, 4) = (v_l + v_r)/2.0;
-            Obj.coeffs(Obj.colons{:}, 1) = (v_l - v_r)/4.0;
-            Obj.coeffs(Obj.colons{:}, 3) = -3 * Obj.coeffs(Obj.colons{:}, 1);
-        end 
+            %   We have the following equations
+            %   b      = (p'(1) - p'(-1))/4
+            %   d      = -b + (p(1) + p(-1))/2
+            %   a      = (p'(1) - p'(-1))/4 - (p(1) - p(-1))/4
+            %   c      = -a + (p(1) - p(-1))/2
+            % Create coefficients
+            Obj.coeffs = zeros([Obj.op_dims 4]);
+
+            Obj.coeffs(Obj.colons{:}, 2) = (vp_r - vp_l)/4.0;
+            Obj.coeffs(Obj.colons{:}, 4) = -Obj.coeffs(Obj.colons{:}, 2) + (v_l + v_r)/2.0;
+            Obj.coeffs(Obj.colons{:}, 1) =  Obj.coeffs(Obj.colons{:}, 2) - (v_l - v_r)/4.0;
+            Obj.coeffs(Obj.colons{:}, 3) = -Obj.coeffs(Obj.colons{:}, 1) + ...
+                (v_r - v_l)/2.0;
+
+            if (nargout > 0)
+                coeffs = Obj.coeffs;
+            end
+        end
 
         function [val, der] = computeWithDer(Obj, x_in)
             n_pts          = Obj.getNPts();
@@ -57,6 +68,7 @@ classdef SmoothStep < Interpolant
                 % Compute the polynomial  terms
                 x_in2    = times(x_in, x_in);
                 x_in3    = times(x_in, x_in2);
+
 
                 ext_dims = length(Obj.op_dims);
                 x_vec    = shiftdim([x_in3, x_in2, x_in, 1], 1-ext_dims);
