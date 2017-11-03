65cb6c0e34e4336876dfe9d63923e53550935682
Archit Gupta <architgupta@berkeley.edu>
2017-11-02 17:11:08 -0700

    smoothing-functions: Tweaked the smoothing example to see how smooth-step works better than BLI-based smoothing
    Changes committed:
    	modified:   smoothing/fxsw_smoothBLI.m

--- matlab/examples/smoothing/fxsw_smoothBLI.m
+++ matlab/examples/smoothing/fxsw_smoothBLI.m
@@ -1,27 +1,30 @@
 disp('SmoothBLI interpolant for smoothing');
 % Discontinuous function
 % [fun, dfun, bkp] = getTestFHandle(1,1,'d');
-step_width      = 1000;
+step_width      = 100000;
+steep_slope     = 10;
 delta           = 0.01;
-fun             = @(x) (1 - x) .* (1 + tanh(step_width*(x-delta)))/2;
-dfun            = @(x) -(1 + tanh(step_width*(x-delta)))/2 + ...
+fun             = @(x) (1 - steep_slope * x) .* (1 + tanh(step_width*(x-delta)))/2;
+dfun            = @(x) -steep_slope * (1 + tanh(step_width*(x-delta)))/2 + ...
     (step_width/2)*(1-x).*(1 - tanh(step_width*(x-delta)).^2);
 
 disp('Generating arguments for the interpolant.');
 args            = defaultPiecewiseInterpolantArgs();
-args{2}         = {[-1.0; -0.1; 0.1; 1.0]};
-args{3}(:)      = 3;
+args{2}         = {[-1.0; 0.0; 0.02; 1.0]};
+args{3}(:)      = 5;
 args{4}         = 'chebyshev';
+
+dcont_bounds    = {args{2}{1}(2:3)};
 
 disp('Instantiating Smooth BLI.')
 % Pass in argument for smoothing
 bl_interpolant  = PiecewiseBLI(fun, args{:}, false);
 
 % Set the middle piece to be a low order spline
-bl_interpolant.setInterpolant(2, @SmoothStep, fun, {[-0.1; 0.1]}, [2]);
+bl_interpolant.setInterpolant(2, @SmoothStep, fun, dcont_bounds, [2]);
 bl_interpolant.ironOut(2);
 
-ss_interpolant  = SmoothStep(fun, 1, {[-0.1; 0.1]}, [2],  'chebyshev');
+ss_interpolant  = SmoothStep(fun, 1, dcont_bounds, [2],  'chebyshev');
 ss_interpolant.fit(dfun(-0.1), dfun(0.1));
 
 % bl_interpolant.plotChebCoeffs(1);
@@ -34,5 +37,5 @@ hod_interpolant = PiecewiseBLI(fun, hod_args{:}, true);
 
 n_test_pts      = 1000;
 bounds          = args{2};
-compareIObjs(n_test_pts, bounds, fun, bl_interpolant, 'SMOOTH-BLI', ...
+compareIObjs(n_test_pts, dcont_bounds, fun, bl_interpolant, 'SMOOTH-BLI', ...
     hod_interpolant, 'SPLINE-BLI');
