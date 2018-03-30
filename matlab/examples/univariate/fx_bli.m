9d0a4a7fb3b55c6c416a15a40f89a1a7d4b74b1e
Archit Gupta <architgupta@berkeley.edu>
2018-03-30 16:13:40 -0700

    master: Changed the Spline code a little to accomodate Passive Spline interpolation that was demonstrated in the thesis. Added this to the test suite as well.
    Changes committed:
    	modified:   matlab/examples/univariate/fx_bli.m
    	modified:   matlab/splines/PassiveSpline1D.m
    	modified:   matlab/splines/Spline1D.m
    	copied:     matlab/splines/Spline1D.m -> matlab/splines/SplineInterpolant.m

--- matlab/examples/univariate/fx_bli.m
+++ matlab/examples/univariate/fx_bli.m
@@ -1,4 +1,4 @@
-disp('Example illustrating the use of Barycentric-Lagrange Interpolant.');
+disp('Example illustrating the use of Splines and Barycentric-Lagrange Interpolant.');
 fun            = getTestFHandle(1, 1, 'smooth');
 
 disp('Generating arguments for the interpolant.');
@@ -6,7 +6,8 @@ args           = defaultInterpolantArgs();
 
 disp('Instantiating BLI.')
 bl_interpolant = BLI(fun, args{:});
+sp_interpolant = Spline1D(fun, args{:});
 
 n_test_pts     = 1000;
 bounds         = args{2};
-compareIObjs(n_test_pts, bounds, fun, bl_interpolant, 'BLI');
+compareIObjs(n_test_pts, bounds, fun, bl_interpolant, 'BLI', sp_interpolant, 'SPLINE');
--- matlab/splines/PassiveSpline1D.m
+++ matlab/splines/PassiveSpline1D.m
@@ -1,7 +1,13 @@
-classdef PassiveSpline1D < Spline1D
+classdef PassiveSpline1D < SplineInterpolant
     methods (Access = public)
-        function obj = PassiveSpline1D(varargin)
-            
+        function Obj = PassiveSpline1D(varargin)
+        % function Obj = PassiveSpline1D(f_vals, n_in_dims, bounds, order, i_type_or_x_vals)
+        % Class Constructor
+            % Based on the sample points and the associated function values,
+            % adjust the value of the terminal slopes.
+            Obj.ROI_length = Obj.i_pts.getPtAt() - Obj.i_pts.getPtAt(1);
+            Obj.MIN_EXTRAP_SLOPE = 0.05*max(abs(Obj.f_vals(Obj.colons{:}, :)), [], 1)/ROI_length;
+            Obj.setupCoefficients()
         end
     end
 end
